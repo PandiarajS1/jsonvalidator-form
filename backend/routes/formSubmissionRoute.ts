900d8af49eac9b0f2932f8fd0b650eabc424bb27
Pandiaraj S <140794266+PandiarajS1@users.noreply.github.com>
2025-06-15 10:45:04 +0530

    Update formSubmissionRoute.ts

--- backend/routes/formSubmissionRoute.ts
+++ backend/routes/formSubmissionRoute.ts
@@ -3,6 +3,6 @@ import { postFormSubmission } from "../controllers/FormSubmissionController";
 
 const router = express.Router();
 
-router.post("/:schemaid", postFormSubmission as RequestHandler);
+router.post("/:schemaid", postFormSubmission as unknown as RequestHandler);
 
 export default router;
